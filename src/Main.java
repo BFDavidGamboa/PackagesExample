88bfc14795e60165f52dc4f5662337c9078bbf96
BFDavidGamboa <david.gamboa@blankfactor.com>
2023-08-17 14:01:14 -0500

    Created package organization and example of imports for static methods and other parameters

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,16 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+//import com.gm.Utils;
+//This will not include the sub packages in the packages
+//import com.gm.*;
+import static com.gm.Utils.print;
 public class Main {
     public static void main(String[] args) {
-        // Press Opt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        //Using the name of the class fully qualified
+        com.gm.Utils.print("nya!");
 
-        // Press Ctrl+R or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        //Using the import of the class
+        //Utils.print("nya nya nya!");
 
-            // Press Ctrl+D to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Cmd+F8.
-            System.out.println("i = " + i);
-        }
+        print("nya nya nya nya!!");
+
     }
 }
---
+++ src/com/gm/Utils.java
@@ -0,0 +1,7 @@
+package com.gm;
+
+public class Utils {
+    public static void print(String s){
+        System.out.println("s = " + s);
+    }
+}
